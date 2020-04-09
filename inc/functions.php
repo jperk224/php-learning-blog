0c7771d308b9462a92836a287b36a8f8bc28886f
Jeff Perkinson <jmperkinson@gmail.com>
2020-04-09 19:56:12 -0400

    Add search functionality to index.php.
    This commit completes search functionality for
    journal entries.  The search query looks in title,
    learned, and resources columns.

--- inc/functions.php
+++ inc/functions.php
@@ -4,10 +4,11 @@
 
 // Pull entires from the database
 // TODO: Add use of limit and offet parameters for pagination
-function getJournalEntries($limit = null, $offset = 0) {
+function getJournalEntries($limit = null, $offset = 0, $searchString = null) {
     try {
         include("inc/connection.php");  // include over require 
                                         // so we don't throw a fatal error and kill the whole page
+        $searchString = strtolower($searchString);
         $sql = "SELECT * FROM entries";
 
         // A prepare statment is used in lieu of $db->query b/c
@@ -15,18 +16,42 @@ function getJournalEntries($limit = null, $offset = 0) {
         // that are derived from query string values; prepare() ensures
         // the query written here is what's truly executed
 
-        // If a limit is specified apped it to the SQL else just use the offset
-        // if limit is an integer, we know one is passed in and its not null
-        if(is_integer($limit)) {
-            $sql .= " LIMIT ? OFFSET ?";
-            $results = $db->prepare($sql);
-            $results->bindParam(1, $limit, PDO::PARAM_INT);
-            $results->bindParam(2, $offset, PDO::PARAM_INT);
+        // if there's a searchString, append the WHERE filter
+        if(!empty($searchString)) {
+            $searchString = "%" . $searchString . "%";  // SQL quotes automatically added by PDO per docs      
+            $sql .= " WHERE title like :searchString1 
+                    OR learned like :searchString2 
+                    OR resources like :searchString3";
+
+            // If a limit is specified apped it to the SQL else just use the offset
+            // if limit is an integer, we know one is passed in and its not null
+            if(is_integer($limit)) {
+                $sql .= " LIMIT :limit OFFSET :offset";
+                $results = $db->prepare($sql);
+                $results->bindParam(':searchString1', $searchString, PDO::PARAM_STR);
+                $results->bindParam(':searchString2', $searchString, PDO::PARAM_STR);
+                $results->bindParam(':searchString3', $searchString, PDO::PARAM_STR);
+                $results->bindParam(':limit', $limit, PDO::PARAM_INT);
+                $results->bindParam(':offset', $offset, PDO::PARAM_INT);
+            }
+            else {  // No limit specified
+                $results = $db->prepare($sql);
+                $results->bindParam(':searchString1', $searchString, PDO::PARAM_STR);
+                $results->bindParam(':searchString2', $searchString, PDO::PARAM_STR);
+                $results->bindParam(':searchString3', $searchString, PDO::PARAM_STR);
+            }
         }
-        // If no limit argument, return all values
         else {
-            $results = $db->prepare($sql);
-        }       
+            if(is_integer($limit)) {    // Limit but no search query
+                $sql .= " LIMIT :limit OFFSET :offset";
+                $results = $db->prepare($sql);
+                $results->bindParam(':limit', $limit, PDO::PARAM_INT);
+                $results->bindParam(':offset', $offset, PDO::PARAM_INT);
+            }
+            else {  // No limit or search query
+                $results = $db->prepare($sql);
+            } 
+        }
         $results->execute();
     }
     catch (Exception $e) {
@@ -40,12 +65,24 @@ function getJournalEntries($limit = null, $offset = 0) {
 
 // For pagination to work, we need to know how many entries are in the database
 // this function counts the number of entires in the entries table by id
-function getJournalEntryCount() {
+function getJournalEntryCount($searchString = null) {
     include("inc/connection.php");
     try {
         $sql = "SELECT count(id)
                 FROM entries";
-        $results = $db->prepare($sql);
+        if(!empty($searchString)) {
+            $searchString = "%" . $searchString . "%";  // SQL quotes automatically added by PDO per docs      
+            $sql .= " WHERE title LIKE :searchString1 
+                    OR learned LIKE :searchString2 
+                    OR resources LIKE :searchString3";
+            $results = $db->prepare($sql);
+            $results->bindParam(':searchString1', $searchString, PDO::PARAM_STR);
+            $results->bindParam(':searchString2', $searchString, PDO::PARAM_STR);
+            $results->bindParam(':searchString3', $searchString, PDO::PARAM_STR);
+        }
+        else {
+            $results = $db->prepare($sql);
+        }
         $results->execute();        
     }
     catch(Exception $e) {
@@ -57,4 +94,23 @@ function getJournalEntryCount() {
     $count = $results->fetchColumn(0);  
 
     return $count;
+}
+
+//--VIEW FUNCTIONS--//
+
+function renderPaginationLinks($currentPage, $totalPages, $searchString = null)
+{
+    // Display links for pagination
+    // If page number link matches current page disable the link
+    for ($i = 1; $i <= $totalPages; $i++) {
+        if ($i == $currentPage) {
+            echo "<li>" . $i . "</li>";
+        } else {
+            if (!empty($searchString)) {
+                echo "<li><a href=\"index.php?page=" . $i . "&searchQuery=" . $searchString . "\">" . $i . "</a></li>";
+            } else {
+                echo "<li><a href=\"index.php?page=" . $i . "\">" . $i . "</a></li>";
+            }
+        }
+    }
 }
