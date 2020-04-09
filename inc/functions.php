51d03841d3508990a9166de8b4ef8260b82c9a64
Jeff Perkinson <jmperkinson@gmail.com>
2020-04-09 11:59:04 -0400

    Add pagination to home page.
    This commit adds pagination to the home page
    to cycle through journal entries via page links.

--- inc/functions.php
+++ inc/functions.php
@@ -4,22 +4,30 @@
 
 // Pull entires from the database
 // TODO: Add use of limit and offet parameters for pagination
-function getJournalEntries($limit = null, $offset = null) {
+function getJournalEntries($limit = null, $offset = 0) {
     try {
         include("inc/connection.php");  // include over require 
                                         // so we don't throw a fatal error and kill the whole page
         $sql = "SELECT * FROM entries";
-        // If an offset is specific append the SQL to include it
-        // if(!empty($offset)) {
-        //     $sql .= " OFFSET ?"
-        // }
 
         // A prepare statment is used in lieu of $db->query b/c
-        // we'll be taking values from the url query string as function
-        // arguments to build our SQL query; prepare() ensures
-        // the query written here is what's executed and prevents SQL injection
-        $results = $db->prepare($sql);
-        //TODO: bind parameters
+        // we'll be taking arguments to build our SQL query
+        // that are derived from query string values; prepare() ensures
+        // the query written here is what's truly executed
+
+        // If a limit is specified apped it to the SQL else just use the offset
+        // if limit is an integer, we know one is passed in and its not null
+        if(is_integer($limit)) {
+            $sql .= " LIMIT ? OFFSET ?";
+            $results = $db->prepare($sql);
+            $results->bindParam(1, $limit, PDO::PARAM_INT);
+            $results->bindParam(2, $offset, PDO::PARAM_INT);
+        }
+        else {
+            $sql .= " OFFSET ?";
+            $results = $db->prepare($sql);
+            $results->bindParam(1, $offset, PDO::PARAM_INT);
+        }       
         $results->execute();
     }
     catch (Exception $e) {
@@ -29,4 +37,25 @@ function getJournalEntries($limit = null, $offset = null) {
     
     // return the results in an associative array so we can leverage column-named keys
     return $results->fetchAll(PDO::FETCH_ASSOC);
+}
+
+// For pagination to work, we need to know how many entries are in the database
+// this function counts the number of entires in the entries table by id
+function getJournalEntryCount() {
+    include("inc/connection.php");
+    try {
+        $sql = "SELECT count(id)
+                FROM entries";
+        $results = $db->prepare($sql);
+        $results->execute();        
+    }
+    catch(Exception $e) {
+        echo $e->getMessage();
+    }
+
+    // our SQL query will return only a single column result with a single row
+    // use fetchColumn index 0 to grab the first column from the result set
+    $count = $results->fetchColumn(0);  
+
+    return $count;
 }
--- index.php
+++ index.php
@@ -1,27 +1,77 @@
 <?php
 
 $pageTitle = "My Journal | Home";
+$itemsPerPage = 5;  // fixed count for display pagination
 
 include("inc/header.php");
 
+// GET variable identifying the pagination page for SQL offsets
+// The variable is taken from the query string, so filter it-- safety first!
+if (isset($_GET["page"])) {
+    $currentPage = filter_input(INPUT_GET, "page", FILTER_SANITIZE_NUMBER_INT);
+}
+
+// If there is no page variable in the query string, current page is empty, 
+// set the current page to 1
+if (empty($currentPage)) {
+    $currentPage = 1;
+}
+
+// this count is needed to determine number of pagination pages
+$totalJournalEntries = getJournalEntryCount();
+
+// determine total pages, round up to ensure there's enough pages to paginate over
+$totalPages = ceil($totalJournalEntries / $itemsPerPage);
+
+// determine the offset for pagination
+// e.g. with 5 entries per page, the offset on page 2 is 5 so page 2 displays entries 6 - 10
+$offset = ($currentPage - 1) * $itemsPerPage;
+
+// Redirect the user to the last page of entries if page in the query string exceeds total pages
+if ($currentPage > $totalPages) {
+    header("location:index.php?page=" . $totalPages);
+}
+
+// Redirect the user to the first page of entries if page in the query string is less than 1
+if ($currentPage < 1) {
+    header("location:index.php?page=1");
+}
+
 ?>
 
-        <section>
-            <div class="container">
-                <div class="entry-list">
-                    <?php
-                        $journalEntries = getJournalEntries();
-                        foreach($journalEntries as $item) {
-                            echo "<article>\n";
-                            echo "<h2><a href=\"#\">" . $item["title"] . "</a></h2>\n";
-                            echo "<time datetime=\"" . $item["date"] . "\">" . $item["date"] . "</time>\n";
-                            echo "</article>\n";
+<section>
+    <div class="container">
+        <div class="entry-list">
+            <div class="pagination-links">
+                <p>Page: </p>
+                <ul>
+                    <?php 
+                    // Display links for pagination
+                    // If page number link matches current page disable the link
+                        for($i = 1; $i <= $totalPages; $i++) {
+                            if($i == $currentPage) {
+                                echo "<li>" . $i . "</li>";
+                            }
+                            else {
+                                echo "<li><a href=\"index.php?page=" . $i . "\">" . $i . "</a></li>";
+                            }
                         }
                     ?>
-                </div>
-            </div>
-        </section>
+                </ul>
+            </div> <!-- end pagination-links -->
+            <?php
+            $journalEntries = getJournalEntries($itemsPerPage, $offset);
+            foreach ($journalEntries as $item) {
+                echo "<article>\n";
+                echo "<h2><a href=\"#\">" . $item["title"] . "</a></h2>\n";
+                echo "<time datetime=\"" . $item["date"] . "\">" . $item["date"] . "</time>\n";
+                echo "</article>\n";
+            }
+            ?>
+        </div>
+    </div>
+</section>
 
-    <?php
-    include("inc/footer.php");
-    ?>
+<?php
+include("inc/footer.php");
+?>
